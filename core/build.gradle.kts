10d9e6e488b2a9bd4a858da69d0cfdc22a08d59f
Mnemotechnician <daytimer253@gmail.com>
2022-12-22 13:23:20 +0300

    migrate to github packages

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -21,13 +21,28 @@ tasks.jar {
 }
 
 publishing {
+	repositories {
+		maven {
+			name = "Github"
+			url = uri("https://maven.pkg.github.com/mnemotechnician/markov-chain")
+			credentials {
+				username = findProperty("github.username") as? String
+					?: System.getenv("GITHUB_USERNAME")
+				password = findProperty("github.token") as? String
+					?: System.getenv("GITHUB_TOKEN")
+			}
+		}
+	}
 	publications {
 		create<MavenPublication>("maven") {
+			pom {
+				url.set("https://github.com/mnemotechnician/markov-chain.git")
+			}
+			from(components["java"])
+
 			groupId = "com.github.mnemotechnician"
 			artifactId = "markov-chain"
-			version = "v1.0"
-
-			from(components["java"])
+			version = findProperty("version") as? String ?: System.getenv("VERSION") ?: "1.0"
 		}
 	}
 }
